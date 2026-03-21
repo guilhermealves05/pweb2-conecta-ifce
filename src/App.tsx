6a44bd9b853568159744ce717963b48c66befd9a
Guilherme Alves dos Santos <guilherme.alves05@aluno.ifce.edu.br>
2026-03-21 17:17:22 -0300

    feat: remove import desnecessário do React no App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import Footer from '@/components/shared/footer'
 import Navbar from '@/components/shared/navbar'
-import { use } from 'react'
-
 import { Outlet } from 'react-router'
 import useScroll from './hooks/useScroll'
 
